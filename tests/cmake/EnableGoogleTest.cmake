d9ca19c095687e17330b9bf8d7eb0ca154940763
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-01-03 18:10:35 -0600

    Don't fetch GoogleTest at while configuring the project.

--- tests/cmake/EnableGoogleTest.cmake
+++ tests/cmake/EnableGoogleTest.cmake
@@ -1,14 +1,40 @@
 # Fetch and build GoogleTest if not present in the system
 if(NOT TARGET GTest)
-    # Download and unpack googletest at configure time
-    configure_file(cmake/GTestRemoteCMakeLists.txt.in ${CMAKE_CURRENT_BINARY_DIR}/googletest/remote/CMakeLists.txt)
-    execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
-        WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/googletest/remote")
-    execute_process(COMMAND "${CMAKE_COMMAND}" --build .
-        WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/googletest/remote")
+    include(ExternalProject)
+    externalproject_add(
+        googletest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG master
+        SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/googletest-src"
+        BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/googletest-build"
+        INSTALL_COMMAND ""
+        TEST_COMMAND ""
+    )
 
-    set(GTEST_ROOT ${CMAKE_BINARY_DIR}/googletest)
-    find_package(GTest REQUIRED)
+    externalproject_get_property(googletest SOURCE_DIR)
+    externalproject_get_property(googletest BINARY_DIR)
+
+    # Workaround fro https://cmake.org/Bug/view.php?id=15052
+    file(MAKE_DIRECTORY "${SOURCE_DIR}/googletest/include/")
+
+    add_library(GTest::GTest UNKNOWN IMPORTED)
+    add_library(GTest::Main UNKNOWN IMPORTED)
+    set_target_properties(
+        GTest::GTest PROPERTIES
+        IMPORTED_LOCATION ${BINARY_DIR}/lib/libgtest.a
+        INTERFACE_INCLUDE_DIRECTORIES "${SOURCE_DIR}/googletest/include/"
+        INTERFACE_LINK_LIBRARIES pthread
+    )
+
+    set_target_properties(
+        GTest::Main PROPERTIES
+        IMPORTED_LOCATION ${BINARY_DIR}/lib/libgtest_main.a
+        INTERFACE_INCLUDE_DIRECTORIES "${SOURCE_DIR}/googletest/include/"
+        INTERFACE_LINK_LIBRARIES pthread
+    )
+
+    add_dependencies(GTest::GTest googletest-remote)
+    add_dependencies(GTest::Main googletest-remote)
 endif()
 
 include(GoogleTest)
