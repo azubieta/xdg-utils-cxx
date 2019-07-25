bd972ce85c00c01c941f38f1472368c9b7f89a1b
杨家烨 <yangjiaye@cvte.com>
2019-07-25 15:09:13 +0800

    Add XdgConfigHome and XdgCacheHome for BaseDir

--- include/XdgUtils/BaseDir/BaseDir.h
+++ include/XdgUtils/BaseDir/BaseDir.h
@@ -21,5 +21,21 @@ namespace XdgUtils {
          * @return XDG_DATA_HOME
          */
         const std::string XdgDataHome();
+
+        /**
+         * Single base directory relative to which user-specific data files should be written.
+         *
+         * See: https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+         * @return XDG_CONFIG_HOME
+         */
+        const std::string XdgConfigHome();
+
+        /**
+         * Single base directory relative to which user-specific data files should be written.
+         *
+         * See: https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+         * @return XDG_CACHE_HOME
+         */
+        const std::string XdgCacheHome();
     }
 }
